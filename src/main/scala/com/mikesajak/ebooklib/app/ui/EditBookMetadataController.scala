2dc1f624d4319f731af71bd61775665b27ac0293
mike <mike.sajak@gmail.com>
2019-04-28 15:32:26 +0200

    Update tooltips initialization - don't expect to explicitly create tooltips in FXML, create them on the fly.

--- src/main/scala/com/mikesajak/ebooklib/app/ui/EditBookMetadataController.scala
+++ src/main/scala/com/mikesajak/ebooklib/app/ui/EditBookMetadataController.scala
@@ -15,73 +15,71 @@ trait EditBookMetadataController {
 
 @sfxml
 class EditBookMetadataControllerImpl(titleTextField: TextField,
-                                     titleTooltip: Tooltip,
                                      authorsCombo: ComboBox[String],
-                                     authorsTooltip: Tooltip,
                                      seriesCombo: ComboBox[String],
-                                     seriesTooltip: Tooltip,
                                      seriesNumSpinner: Spinner[Int],
                                      tagsCombo: ComboBox[String],
-                                     tagsTooltip: Tooltip,
                                      identifiersTextField: TextField,
-                                     identifiersTooltip: Tooltip,
                                      creationDateTextField: TextField,
                                      creationDateSelButton: Button,
                                      publicationDateTextField: TextField,
                                      publicationDateSelButton: Button,
                                      publisherCombo: ComboBox[String],
-                                     publisherTooltip: Tooltip,
                                      languagesCombo: ComboBox[String],
-                                     languagesTooltip: Tooltip,
                                      coverImageView: ImageView)
     extends EditBookMetadataController {
 
-  creationDateSelButton.margin = Insets(0,0,0,0)
-  creationDateSelButton.padding = Insets(0,0,0,0)
-  publicationDateSelButton.margin = Insets(0,0,0,0)
-  publicationDateSelButton.padding = Insets(0,0,0,0)
+  zeroMargins(creationDateSelButton, publicationDateSelButton)
 
   seriesNumSpinner.valueFactory = new IntegerSpinnerValueFactory(0, 100, 0).asInstanceOf[SpinnerValueFactory[Int]]
 
   def initialize(book: BookMetadata, cover: Option[Image]): Unit = {
     titleTextField.text = book.getTitle
-    initTextFieldTooltip(titleTextField, titleTooltip)
+    initTextFieldTooltip(titleTextField)
     authorsCombo.value = strVal(book.getAuthors, " & ")
-    initComboTooltip(authorsCombo, authorsTooltip, "\\s*&\\s*")
+    initComboTooltip(authorsCombo, "\\s*&\\s*")
 
     seriesNumSpinner.disable = true
     seriesCombo.editor.value.textProperty().addListener{ (_, _, newValue) =>
       seriesCombo.value = newValue
       seriesNumSpinner.disable = Option(seriesCombo.value.value).forall(_.isEmpty)
     }
-    initComboTooltip(seriesCombo, seriesTooltip)
+    initComboTooltip(seriesCombo)
     Option(book.getSeries).foreach { series =>
       seriesCombo.value = series.getTitle
       seriesNumSpinner.valueFactory.value.setValue(series.getNumber)
     }
 
     tagsCombo.value = strVal(book.getTags, ", ")
-    initComboTooltip(tagsCombo, tagsTooltip, "\\s*,\\s*")
+    initComboTooltip(tagsCombo, "\\s*,\\s*")
     identifiersTextField.text = strVal(book.getIdentifiers, ", ")
-    initTextFieldTooltip(identifiersTextField, identifiersTooltip, "\\s*,\\s*")
+    initTextFieldTooltip(identifiersTextField, "\\s*,\\s*")
     creationDateTextField.text = strVal(book.getCreationDate)
     publicationDateTextField.text = strVal(book.getPublicationDate)
     publisherCombo.value = book.getPublisher
-    initComboTooltip(publisherCombo, publisherTooltip )
+    initComboTooltip(publisherCombo)
     languagesCombo.value = strVal(book.getLanguages, ", ")
-    initComboTooltip(languagesCombo, languagesTooltip, "\\s*,\\s*")
+    initComboTooltip(languagesCombo, "\\s*,\\s*")
 
     for (image <- cover) coverImageView.image = image
   }
 
-  private def initTextFieldTooltip(textControl: TextInputControl, tooltip: Tooltip, separator: String = "^"): Unit =
-    initTooltipImpl(() => strVal(textControl.text.value), tooltip, separator)
+  private def zeroMargins(buttons: Button*): Unit =
+    buttons.foreach { b =>
+      b.margin = Insets(0,0,0,0)
+      b.padding = Insets(0,0,0,0)
+    }
 
-  private def initComboTooltip(combo: ComboBox[_], tooltip: Tooltip, separator: String = "^"): Unit =
-    initTooltipImpl(() => strVal(combo.value.value), tooltip, separator)
+  private def initTextFieldTooltip(textControl: TextInputControl, separator: String = "^"): Unit =
+    initTooltipImpl(() => strVal(textControl.text.value), textControl, separator)
 
-  private def initTooltipImpl(textFun: () => String, tooltip: Tooltip, separator: String = "^"): Unit = {
+  private def initComboTooltip(combo: ComboBox[_], separator: String = "^"): Unit =
+    initTooltipImpl(() => strVal(combo.value.value), combo, separator)
+
+  private def initTooltipImpl(textFun: () => String, control: Control, separator: String = "^"): Unit = {
+    if (control.tooltip.value == null) control.tooltip = ""
     val elements = textFun().split(separator)
+    val tooltip: Tooltip = new Tooltip(control.tooltip.value)
     tooltip.onShowing = _ => tooltip.text = elements.reduce{(elem1, elem2) => s"$elem1\n$elem2"}
   }
 
